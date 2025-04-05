82563bc1c319104fe772711d98fff96ad4e38369
A3 <warmerdamm03@gmail.com>
2025-04-05 21:35:12 +0200

    almost all functionality, except for sms data, waiting for tomorrow for a bill that includes sms so that i can build it into python (receipt is needed to get correct regex)

--- app.py
+++ app.py
@@ -1,16 +1,137 @@
 import os
 import pypdf
 import re
+import pandas as pd
+import glob
+import datetime
+import matplotlib.pyplot as plt
+
 
 debug = "true"
 
 def generate_graphs():
     if debug == "true":
         print("Starting Generating Graphs")
+        
+    dfphone = pd.read_csv("data/phone-out.csv")
+    def convert_to_minutes(time_str):
+        h, m, s = map(int, time_str.split(':'))
+        return h * 60 + m + s / 60
+    
+    dfphone['total_minutes'] = dfphone['lenght'].apply(convert_to_minutes)
+    dfphone.to_csv('data/phone-out.csv', index=False)
+
+    def generate_dayoftheweek_mbs():
+        dfmbs = pd.read_csv("data/mbs-out.csv")
+        grouped = dfmbs.groupby('day_of_week')['amount'].sum()
+        days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        grouped = grouped.reindex(days_order)
+
+        # Plot the data using matplotlib
+        plt.figure(figsize=(10, 6))
+        grouped.plot(kind='bar', color=['#003f5c','#374c80',"#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"])
+        #003f5c
+
+        plt.title('Data Usage per Day of the Week')
+        plt.xlabel('Day of the Week')
+        plt.ylabel('Amount (MB)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.style.use('dark_background')
+        plt.savefig('output/dayoftheweek_mbs.png')
+
+    def generate_month_mbs():
+        dfmbs = pd.read_csv("data/mbs-out.csv")
+        dfmbs['date'] = pd.to_datetime(dfmbs['date'], format='%d-%m-%Y')
+        # Add a new column with the month
+        dfmbs['month'] = dfmbs['date'].dt.strftime('%B')
+        grouped = dfmbs.groupby('month')['amount'].sum()
+        days_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        grouped = grouped.reindex(days_order)
+
+        # Plot the data using matplotlib
+        plt.figure(figsize=(10, 6))
+        grouped.plot(kind='bar', color=['#003f5c','#374c80',"#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"])
+        #003f5c
+
+        plt.title('Data Usage per Month')
+        plt.xlabel('Month')
+        plt.ylabel('Amount (MB)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.style.use('dark_background')
+        plt.savefig('output/month_mbs.png')
+
+    def generate_dayoftheweek_phone():
+        dfphone = pd.read_csv("data/phone-out.csv")
+
+#            def convert_to_minutes(time_str):
+#                h, m, s = map(int, time_str.split(':'))
+#                return h * 60 + m + s / 60
+        
+#            dfphone['total_minutes'] = dfphone['lenght'].apply(convert_to_minutes)
+
+        grouped = dfphone.groupby('day_of_week')['total_minutes'].sum()
+        days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        grouped = grouped.reindex(days_order)
+
+        # Plot the data using matplotlib
+        plt.figure(figsize=(10, 6))
+        grouped.plot(kind='bar', color=['#003f5c','#374c80',"#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"])
+        #003f5c
+
+        plt.title('Phone Calls Minutes per Day of the Week')
+        plt.xlabel('Day of the Week')
+        plt.ylabel('Amount (Minutes)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.style.use('dark_background')
+        plt.savefig('output/dayoftheweek_phone.png')
+    
+    def generate_month_phone():
+        dfphone = pd.read_csv("data/phone-out.csv")
+        dfphone['date'] = pd.to_datetime(dfphone['date'], format='%d-%m-%Y')
+        # Add a new column with the month
+        dfphone['month'] = dfphone['date'].dt.strftime('%B')
+        grouped = dfphone.groupby('month')['total_minutes'].sum()
+        days_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        grouped = grouped.reindex(days_order)
+
+        # Plot the data using matplotlib
+        plt.figure(figsize=(10, 6))
+        grouped.plot(kind='bar', color=['#003f5c','#374c80',"#7a5195","#bc5090","#ef5675","#ff764a","#ffa600"])
+        #003f5c
+
+        plt.title('Data Usage per Month')
+        plt.xlabel('Month')
+        plt.ylabel('Amount (MB)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.style.use('dark_background')
+        plt.savefig('output/month_phone.png')
+    
+    generate_dayoftheweek_mbs()
+    generate_month_mbs()
+    generate_dayoftheweek_phone()
+    generate_month_phone()
 
 def sort_data_per_month():
     if debug == "true":
-        print("Starting sorting data per month")
+        print("Starting sorting all data")
+
+    dfphone = pd.concat(map(pd.read_csv, glob.glob('data/rawdata/gesprekken*.csv')))
+    dfphone['date_converted'] = pd.to_datetime(dfphone['date'], format='%d-%m-%Y')
+    dfphone['day_of_week'] = dfphone['date_converted'].dt.day_name() # Create a new column with the day of the week
+    dfphone.drop(columns=['date_converted'], inplace=True) # Drop the intermediate datetime conversion column because it is not needed
+    dfphone.to_csv('data/phone-out.csv', index=False)
+    print (dfphone)
+
+    dfmbs = pd.concat(map(pd.read_csv, glob.glob('data/rawdata/mbs*.csv')))
+    dfmbs['date_converted'] = pd.to_datetime(dfmbs['date'], format='%d-%m-%Y')
+    dfmbs['day_of_week'] = dfmbs['date_converted'].dt.day_name() # Create a new column with the day of the week
+    dfmbs.drop(columns=['date_converted'], inplace=True) # Drop the intermediate datetime conversion column because it is not needed
+    print(dfmbs)
+    dfmbs.to_csv('data/mbs-out.csv', index=False)
     generate_graphs()
 
 def extract_text_from_all_pages(pdf_path):
@@ -53,7 +174,7 @@ def extract_text_from_pdfs_in_folder(folder_path):
 
             filename = pdf_path[:-4]
             filename = filename[-6:]
-            rawdatafilename = "rawdata/"+filename+".txt"
+            rawdatafilename = "data/rawdata/"+filename+".txt"
             f = open(rawdatafilename, "w")
             f.write(text)
             f.close()
@@ -61,7 +182,7 @@ def extract_text_from_pdfs_in_folder(folder_path):
                 print("rawdatafilename:",rawdatafilename)    
             
             
-            gesprekkenfilename = "rawdata/"+filename+"-gesprekken.csv"
+            gesprekkenfilename = "data/rawdata/gesprekken-"+filename+".csv"
             if debug == "true":
                 print("gesprekkenfilename:",gesprekkenfilename)
 
@@ -72,29 +193,27 @@ def extract_text_from_pdfs_in_folder(folder_path):
                 x = re.findall("(^[0-9*]{11}[\s]{1})", line)
             
                 if x:
-                
                     if debug == "true":
                         # printing those lines
                         print(line)
-                    
-                    # storing only those lines that 
-                    # do not begin with "TextGenerator"
+                    # storing only those lines that do not begin with "TextGenerator"
 
                     gesprekken.write(line)
-                
             gesprekken.close()
 
-            # maakt er maar gelijk een csv van voor table?
+            # maakt er maar gelijk een csv van voor table
             with open(gesprekkenfilename, 'r') as gesprekkenfile:
                 gesprekkeninhoud = gesprekkenfile.read()
                 gesprekkeninhoud = gesprekkeninhoud.replace(" ",",")
 
             with open(gesprekkenfilename, 'w') as gesprekkenfile:
                 # write csv collums
-                gesprekkenfile.write("number,date,time,lenght,bundel\n")
+                gesprekkenfile.write("number,date,time,lenght,bundle_or_price\n")
                 gesprekkenfile.write(gesprekkeninhoud)
 
-            mbsfilename = "rawdata/"+filename+"-mbs.csv"
+                dfphone = pd.read_csv("data/phone-out.csv")
+
+            mbsfilename = "data/rawdata/mbs-"+filename+".csv"
             if debug == "true":
                 print("mbsfilename:",mbsfilename)
 
@@ -105,33 +224,25 @@ def extract_text_from_pdfs_in_folder(folder_path):
                 x = re.findall(r"[MB]{2}\sc", line)
             
                 if x:
-                
                     if debug == "true":
                         # printing those lines
                         print(line)
-                    
-                    # storing only those lines that 
-                    # do not begin with "TextGenerator"
+                    # storing only those lines that do not begin with "TextGenerator"
 
                     mbs.write(line)
-                
             mbs.close()
 
-                        # maakt er maar gelijk een csv van voor table?
+            # maakt er maar gelijk een csv van voor table
             with open(mbsfilename, 'r') as mbsfile:
                 mbsinhoud = mbsfile.read()
                 mbsinhoud = mbsinhoud.replace(",",".")
                 mbsinhoud = mbsinhoud.replace(" ",",")
-                mbsinhoud = mbsinhoud.replace(",MB"," MB")
+                mbsinhoud = mbsinhoud.replace(",MB","")
 
             with open(mbsfilename, 'w') as mbsfile:
                 # write csv collums
-                mbsfile.write("date,time,amount,bundel\n")
+                mbsfile.write("date,time,amount,bundle_or_price\n")
                 mbsfile.write(mbsinhoud)
-
-            ##regex for date ([0-9]{2}[-]{1}[0-9]{2}[-]{1}[0-9]{4})
-            ##regex to get call time ([0-9]{2}[:]{1}[0-9]{2}[:]{1}[0-9]{2}[ ]{1}[c]{1})
-            ##regex to get mb's used ([,]{1}[0-9]{2})
 
             sort_data_per_month()
             
